056c13be336d56896cf35607f8d65c5d339db51d
beenow011 <abhinavnb11@gmail.com>
2024-10-08 12:52:32 +0530

    Refactor finalizeSale function and add tests for sale completion

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -82,4 +82,21 @@ contract Escrow {
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
+
+    function finalizeSale(uint256 _nftID) public {
+        require(inspectionPassed[_nftID]);
+        require(approval[_nftID][buyer[_nftID]]);
+        require(approval[_nftID][seller]);
+        require(approval[_nftID][lender]);
+        require(address(this).balance >= purchasePrice[_nftID]);
+
+        isListed[_nftID] = false;
+
+        (bool success, ) = payable(seller).call{value: address(this).balance}(
+            ""
+        );
+        require(success);
+
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+    }
 }
